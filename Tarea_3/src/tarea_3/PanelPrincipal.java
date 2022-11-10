21c73472aa19056473861ef95e3a96e5a7a958d4
Keyteer <>
2022-11-10 20:46:25 -0300

    PanelPrincipal transformado a LayeredPane

--- Tarea_3/src/tarea_3/PanelPrincipal.java
+++ Tarea_3/src/tarea_3/PanelPrincipal.java
@@ -3,7 +3,7 @@ package tarea_3;
 import java.util.ArrayList;
 import java.awt.*;
 
-public class PanelPrincipal extends javax.swing.JPanel {
+public class PanelPrincipal extends javax.swing.JLayeredPane {
 
     private ArrayList<Moneda_GUI> depositoMonedas;
     private Comprador compr;
@@ -12,6 +12,7 @@ public class PanelPrincipal extends javax.swing.JPanel {
         depositoMonedas = new ArrayList<Moneda_GUI>();
         initComponents();
         compr = new Comprador(null, exp_GUI.getExpendedor(), 0);
+        this.setLayer(moneda_GUI1, DEFAULT_LAYER);
     }
 
     @Override
@@ -24,7 +25,6 @@ public class PanelPrincipal extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        layerdPane = new javax.swing.JLayeredPane();
         exp_GUI = new tarea_3.Expendedor_GUI();
         moneda_GUI1 = new tarea_3.Moneda_GUI();
         moneda_GUI2 = new tarea_3.Moneda_GUI();
@@ -38,80 +38,50 @@ public class PanelPrincipal extends javax.swing.JPanel {
             }
         });
 
-        layerdPane.setBackground(new java.awt.Color(242, 69, 242));
-        layerdPane.setPreferredSize(new java.awt.Dimension(500, 500));
-        layerdPane.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                layerdPaneMouseClicked(evt);
-            }
-        });
-
         moneda_GUI2.setMoneda(new Moneda500());
 
         moneda_GUI3.setMoneda(new Moneda1000());
 
         moneda_GUI4.setMoneda(new Moneda1500());
 
-        layerdPane.setLayer(exp_GUI, javax.swing.JLayeredPane.DEFAULT_LAYER);
-        layerdPane.setLayer(moneda_GUI1, 2);
-        layerdPane.setLayer(moneda_GUI2, 2);
-        layerdPane.setLayer(moneda_GUI3, 2);
-        layerdPane.setLayer(moneda_GUI4, 2);
-
-        javax.swing.GroupLayout layerdPaneLayout = new javax.swing.GroupLayout(layerdPane);
-        layerdPane.setLayout(layerdPaneLayout);
-        layerdPaneLayout.setHorizontalGroup(
-            layerdPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layerdPaneLayout.createSequentialGroup()
-                .addGap(38, 38, 38)
-                .addComponent(exp_GUI, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGroup(layerdPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layerdPaneLayout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(moneda_GUI2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layerdPaneLayout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(moneda_GUI1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layerdPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(moneda_GUI3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(moneda_GUI4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(92, 92, 92))
-        );
-        layerdPaneLayout.setVerticalGroup(
-            layerdPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layerdPaneLayout.createSequentialGroup()
-                .addGap(123, 132, Short.MAX_VALUE)
-                .addGroup(layerdPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(exp_GUI, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layerdPaneLayout.createSequentialGroup()
-                        .addGroup(layerdPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(moneda_GUI4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layerdPaneLayout.createSequentialGroup()
-                                .addGroup(layerdPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(moneda_GUI1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(moneda_GUI3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(moneda_GUI2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(114, 114, 114)))
-                .addGap(76, 76, 76))
-        );
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(layerdPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(65, 65, 65)
+                .addComponent(exp_GUI, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 133, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(moneda_GUI1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(moneda_GUI4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(moneda_GUI2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(moneda_GUI3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(58, 58, 58))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(layerdPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(139, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(moneda_GUI2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(moneda_GUI3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(moneda_GUI4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(moneda_GUI1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(154, 154, 154))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(exp_GUI, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(69, 69, 69))))
         );
     }// </editor-fold>//GEN-END:initComponents
-
-    private void layerdPaneMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_layerdPaneMouseClicked
-        System.out.println(evt.getX() + "  " + evt.getY());
-    }//GEN-LAST:event_layerdPaneMouseClicked
 
     private void formMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseReleased
         Moneda_GUI m;
@@ -122,7 +92,7 @@ public class PanelPrincipal extends javax.swing.JPanel {
             if (exp_GUI.isInCoinSlot(m.getCenter()) && compr.getMoneda() == null) {
 
                 compr.setMoneda(m.getMoneda());
-                layerdPane.remove(m);
+                this.remove(m);
                 this.repaint();
                 System.out.println("removido ");
             }
@@ -132,7 +102,6 @@ public class PanelPrincipal extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private tarea_3.Expendedor_GUI exp_GUI;
-    private javax.swing.JLayeredPane layerdPane;
     private tarea_3.Moneda_GUI moneda_GUI1;
     private tarea_3.Moneda_GUI moneda_GUI2;
     private tarea_3.Moneda_GUI moneda_GUI3;
